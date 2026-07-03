3fcda511e0df3d9504790ea60cd924dad85f17f9
shu.yamaga <s.yamaga.0318@gmail.com>
2026-07-03 10:13:09 +0900

    RAG: DB 作成スクリプトの作成
    FAISS でベクトル化したデータをローカル保存

--- joseito/mk-rag-db-from-text.py
+++ joseito/mk-rag-db-from-text.py
@@ -1,11 +1,24 @@
-with open('joseito.txt', 'r', encoding='utf-8') as f:
+# Step1. パッセージへの分解
+with open('joseito/joseito.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
 
 text_splitter = RecursiveCharacterTextSplitter(
     chunk_size = 100, #チャンクの文字数
     chunk_overlap = 0, #チャンクオーバーラップの文字数
 )
 
-texts = text_splitter.split_text(text) 
+passages = text_splitter.split_text(text) 
+
+# Step2. パッセージのベクトル化
+embeddings = HuggingFaceEmbeddings(
+    model_name = "intfloat/multilingual-e5-large",
+    model_kwargs = {'device':'mps'}
+)
+
+# Step3. DBの作成
+db = FAISS.from_texts(passages, embeddings)
+db.save_local('joseito/joseito.db')
